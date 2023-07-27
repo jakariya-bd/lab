20bf63be1970a2148e8ad4591584ea0634cde45c
MD Jakariya Molla <121351153+jakariya-bd@users.noreply.github.com>
2023-07-27 21:54:35 +0600

    Add files via upload

--- ANSI-C_Examples/6.03.c
+++ ANSI-C_Examples/6.03.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+main()
+{
+    int A, B, C, X;
+    printf("Enter three values\n");
+    scanf("%d %d %d", &A, &B, &C);
+    printf("\nLargest value is ");
 
+    if( A > B ){
+        if( A > C ) X = A;
+        else X = C;
+    }
+    else if( B > C ) X = B;
+    else X = C;
+
+    printf("%d\n", X);
+
+}
